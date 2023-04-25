fd51d1e4e9074afd180712e1832039e33420c3b6
omer akkaya <farukakkayaya51@gmail.com>
2023-04-25 15:57:21 +0300

    added public folder

---
+++ public/CategoryArray.js
@@ -0,0 +1,11 @@
+const CategoryArray = [
+  { id: 1, title: "Avantajlı Paketler", image: "/../public/cipss.png" },
+  { id: 2, title: "Mix Paketler", image: "/../public/cipss.png" },
+  { id: 3, title: "Granola & Yulaf Ezmesi", image: "/../public/cipss.png" },
+  { id: 4, title: "Kinoa Cips", image: "/../public/cipss.png" },
+  { id: 5, title: "Raw Bites", image: "/../public/cipss.png" },
+  { id: 6, title: "Cracks", image: "/../public/cipss.png" },
+  { id: 7, title: "Glutensiz Unlar", image: "/../public/cipss.png" },
+];
+
+export default CategoryArray;
---
+++ public/popularProducts.js
@@ -0,0 +1,6 @@
+export const popularProducts = [
+  { title: "9'lu Tanışma Paketi", price: "265,00 TL", photoUrl: "" },
+  { title: "Glutensiz Parti Paketi", price: "169,00 TL", photoUrl: "" },
+  { title: "15'li Mega Paket", price: "545,00 TL", photoUrl: "" },
+  { title: "Glutensiz Kahvaltı Paketi", price: "168,00 TL", photoUrl: "" },
+];
