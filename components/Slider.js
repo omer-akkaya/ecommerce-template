2e5bd9714c363d4a3b0a2b95945052378043087f
omer akkaya <farukakkayaya51@gmail.com>
2023-04-25 15:56:54 +0300

    added Slider component

---
+++ components/Slider.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Slider = () => {
+  return (
+    <div className="max-w-full mx-24 h-[600px] bg-gray-400 flex justify-center items-center">
+      Slider
+    </div>
+  );
+};
+
+export default Slider;
