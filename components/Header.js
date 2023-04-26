16ce481776cbe80b9d57acbd1f9d1acedec7a12f
omer akkaya <farukakkayaya51@gmail.com>
2023-04-26 17:05:49 +0300

    updated header

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,9 @@
+import Image from "next/image";
 import { AiOutlineStar } from "react-icons/ai";
 import { AiOutlineUser } from "react-icons/ai";
 import { AiOutlineSearch } from "react-icons/ai";
 import { BsBasket3Fill } from "react-icons/bs";
+import { GoThreeBars } from "react-icons/go";
 
 const Header = () => {
   return (
@@ -9,15 +11,19 @@ const Header = () => {
       <Logo />
       <SearchBox />
       <Icons />
+      <Drawer />
     </div>
   );
 };
 
 const Logo = () => {
   return (
-    <span className="text-4xl font-semibold w-2/12  text-sky-700 font-sans select-none cursor-pointer">
-      Logo
-    </span>
+    <Image
+      src={"/../public/logo.jpeg"}
+      width={200}
+      height={200}
+      className="w-36 mr-16 cursor-pointer select-none ml-4 md:ml-0"
+    />
   );
 };
 
@@ -28,9 +34,9 @@ const SearchBox = () => {
       <input
         type="text"
         placeholder={placeholder}
-        className="px-6 py-2 outline-none rounded-md w-7/12 border-[1px] border-sky-200 shadow-sm focus:border-black"
+        className="hidden md:block px-6 py-2 outline-none rounded-md w-7/12 border-[1px] border-sky-200 shadow-sm focus:border-black"
       ></input>
-      <AiOutlineSearch className="text-3xl relative right-10" />
+      <AiOutlineSearch className="hidden md:block text-3xl relative right-10" />
     </>
   );
 };
@@ -47,9 +53,15 @@ const Icons = () => {
 
 const Circle = ({ icon }) => {
   return (
-    <div className="bg-sky-100 hover:bg-sky-700 hover:text-white cursor-pointer transition-all rounded-full p-3 text-2xl">
+    <div className="hidden md:block bg-sky-100 hover:bg-sky-700 hover:text-white cursor-pointer transition-all rounded-full p-3 text-2xl">
       {icon}
     </div>
+  );
+};
+
+const Drawer = () => {
+  return (
+    <GoThreeBars className="md:hidden text-5xl mr-4 hover:bg-sky-500 hover:text-white rounded-full p-2 transition-all cursor-pointer" />
   );
 };
 
