6ec8b6b4219d0a33bcd44a05a51d00aa14fbce61
omer akkaya <farukakkayaya51@gmail.com>
2023-04-26 17:07:10 +0300

    updated shorts

--- components/Shorts.js
+++ components/Shorts.js
@@ -1,21 +1,25 @@
-import React from "react";
 import CategoryArray from "@/public/CategoryArray";
+import Image from "next/image";
 
 const Shorts = () => {
   return (
-    <div className="max-w-6xl mx-auto py-4 flex justify-between">
-      {CategoryArray.map(({ title }) => {
-        return <SingleShort title={title} />;
+    <div className="max-w-6xl mx-auto py-4 flex justify-between overflow-scroll md:overflow-auto">
+      {CategoryArray.map(({ title, shorts }) => {
+        return <SingleShort title={title} shorts={shorts} />;
       })}
     </div>
   );
 };
 
-const SingleShort = ({ title }) => {
+const SingleShort = ({ title, shorts }) => {
   return (
-    <div className="flex flex-col w-min justify-center items-center">
-      <div className="p-16 bg-sky-500 rounded-full my-3"></div>
-      <div className="whitespace-nowrap text-sky-900 text-sm">{title}</div>
+    <div className="flex flex-col w-min justify-center items-center group cursor-pointer">
+      <div className="flex justify-center items-center w-36 h-36 bg-sky-500 group-hover:bg-sky-900 group-hover:ring-4 group-hover:ring-black transition-all rounded-full p-3 mb-4">
+        <Image src={shorts} width={1000} height={1000} />
+      </div>
+      <div className="whitespace-nowrap text-sky-900 text-sm group-hover:text-black group-hover:underline">
+        {title}
+      </div>
     </div>
   );
 };
