3a57281318e0d4cf8820e8c3525d6fa2c801b107
omer akkaya <farukakkayaya51@gmail.com>
2023-04-26 17:04:45 +0300

    updated Banner

--- components/Banners.js
+++ components/Banners.js
@@ -1,20 +1,28 @@
+import Image from "next/image";
 import React from "react";
 
 const Banners = () => {
   return (
-    <div className="max-w-6xl py-16 mx-auto flex justify-center items-center">
-      <Banner />
-      <Banner />
-      <Banner />
-      <Banner />
+    <div className="max-w-6xl py-10 mx-auto flex justify-center items-center">
+      <Banner id={1} />
+      <Banner id={2} />
+      <Banner id={3} />
+      <Banner id={4} />
     </div>
   );
 };
 
-const Banner = () => {
+function Banner({ id }) {
   return (
-    <div className="bg-red-100 border-2 border-black px-36 py-44 w-min"></div>
+    <div className="w-1/4 cursor-pointer relative group">
+      <div
+        className={`w-full h-full bg-black opacity-40 z-40 absolute hidden group-hover:block`}
+      >
+        adss
+      </div>
+      <Image src={`/../public/banner_${id}.jpeg`} width={1000} height={1000} />
+    </div>
   );
-};
+}
 
 export default Banners;
