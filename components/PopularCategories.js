71cf2fd6ad9ff3396c7ab6d2d4d481838475ccfd
omer akkaya <farukakkayaya51@gmail.com>
2023-04-26 17:06:47 +0300

    updated popularcategories

--- components/PopularCategories.js
+++ components/PopularCategories.js
@@ -1,64 +1,87 @@
+import { useState } from "react";
+
 const PopularCategories = () => {
   return (
     <div>
       <Title />
       <Categories />
-      <Products />
     </div>
   );
 };
 
 const Title = () => {
   return (
-    <div className="max-w-6xl text-2xl mx-auto flex justify-center items-center mb-5">
-      Popüler Kategoriler
+    <div className="max-w-6xl text-2xl mx-auto flex justify-center items-center mb-4 text-sky-950">
+      <span className="border-b-4 border-dotted border-sky-900 w-80 text-center pb-4">
+        Popüler Kategoriler
+      </span>
     </div>
   );
 };
 
 const Categories = () => {
+  const [selected, setSelected] = useState(1);
   return (
-    <div className="max-w-6xl text-2xl mx-auto flex justify-center items-center gap-x-10">
-      <Category title={"Avantajlı Paketler"} />
-      <Category title={"Granola & Yulaf Ezmesi"} />
-      <Category title={"Kinoa Cips"} />
-      <Category title={"Raw Bites"} />
-      <Category title={"Cracks"} />
-    </div>
+    <>
+      <div className="flex md:hidden justify-center items-center">
+        <select className="bg-sky-500 w-3/4 px-8 py-4 rounded-3xl text-white">
+          <option>Avantajlı Paketler</option>
+          <option>Granola & Yulaf Ezmesi</option>
+          <option>Kinoa Cips</option>
+          <option>Raw Bites</option>
+        </select>
+      </div>
+
+      <div className="hidden md:flex max-w-6xl text-2xl mx-auto justify-center items-center gap-x-10 px-5 lg:px-0">
+        <Category
+          title={"Avantajlı Paketler"}
+          id={1}
+          selected={selected}
+          setSelected={setSelected}
+        />
+        <Category
+          title={"Granola & Yulaf Ezmesi"}
+          id={2}
+          selected={selected}
+          setSelected={setSelected}
+        />
+        <Category
+          title={"Kinoa Cips"}
+          id={3}
+          selected={selected}
+          setSelected={setSelected}
+        />
+        <Category
+          title={"Raw Bites"}
+          id={4}
+          selected={selected}
+          setSelected={setSelected}
+        />
+        <Category
+          title={"Cracks"}
+          id={5}
+          selected={selected}
+          setSelected={setSelected}
+        />
+      </div>
+    </>
   );
 };
 
-const Category = ({ title }) => {
+const Category = ({ title, id, selected, setSelected }) => {
+  function clickHandler() {
+    setSelected(id);
+  }
   return (
-    <div className="border-2 border-sky-500 text-sky-500 text-base rounded-3xl py-2 px-8 mb-4 hover:bg-sky-500 hover:text-white transition-all cursor-pointer select-none">
+    <div
+      onClick={clickHandler}
+      className={`text-sky-500 text-base rounded-3xl py-2 px-8 mb-4 hover:bg-sky-500 hover:text-white transition-all cursor-pointer select-none ${
+        selected == id ? "bg-sky-500 text-white" : ""
+      }`}
+    >
       {title}
     </div>
   );
 };
 
-const Products = () => {
-  return (
-    <div className="max-w-6xl text-2xl mx-auto flex justify-center items-center">
-      <Product />
-      <Product />
-    </div>
-  );
-};
-
-const Product = () => {
-  return (
-    <div className="bg-gray-100">
-      <div>Foto</div>
-      <div>9lu tanaışma paketi</div>
-      <div>265,00 TL</div>
-    </div>
-  );
-};
 export default PopularCategories;
-
-const popularProducts = [
-  { title: "9'lu Tanışma Paketi", price: "265,00 TL", photoUrl: "" },
-  { title: "Glutensiz Parti Paketi", price: "169,00 TL", photoUrl: "" },
-  { title: "15'li Mega Paket", price: "545,00 TL", photoUrl: "" },
-  { title: "Glutensiz Kahvaltı Paketi", price: "168,00 TL", photoUrl: "" },
-];
