6d7b40309630dc4345361eb2cd23e3f1c4dea01c
omer akkaya <farukakkayaya51@gmail.com>
2023-04-26 17:04:32 +0300

    updated Announcement

--- components/Announcement.js
+++ components/Announcement.js
@@ -1,17 +1,17 @@
-import Image from "next/image";
+import { TbTruckDelivery } from "react-icons/tb";
+import { HiDocumentSearch } from "react-icons/hi";
 
 const Announcement = () => {
   return (
     <div>
-      <div className="w-full bg-sky-100 text-gray-700 h-10 flex justify-center items-center">
-        <div className="max-w-6xl flex-1 text-sm flex justify-between">
-          <span className="flex items-center gap-x-2">
-            <Image src={"/../public/1.png"} width={25} height={25} />
-            250 TL ve üzeri alışverişlerinizde kargo ücretsiz!
+      <div className="w-full bg-sky-100 text-gray-400 h-14 flex justify-center items-center">
+        <div className="max-w-6xl flex-1 text-sm flex md:justify-between justify-center px-5 lg:px-0">
+          <span className="flex items-center gap-x-2 hover:text-sky-900 transition-all select-none cursor-pointer hover:underline ">
+            <TbTruckDelivery className="text-3xl mr-1" /> 250 TL ve üzeri
+            alışverişlerinizde kargo ücretsiz!
           </span>
-          <span className="flex items-center gap-x-2">
-            <Image src={"/../public/2.png"} width={20} height={20} />
-            Kargom nerede?
+          <span className="md:flex items-center gap-x-2 hover:text-sky-900 transition-all select-none cursor-pointer hover:underline hidden">
+            <HiDocumentSearch className="text-2xl mr-1" /> Kargom nerede?
           </span>
         </div>
       </div>
