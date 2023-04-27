6b73c94fce3279c77769bcbeeda0a460cb0bc6d1
omer akkaya <farukakkayaya51@gmail.com>
2023-04-27 11:29:31 +0300

    updated photo urls

--- public/infoCardsSrc.js
+++ public/infoCardsSrc.js
@@ -1,26 +1,26 @@
 export const infoCardsSrc = [
   {
     title: "Koruyucu İçermez",
-    imageUrl: "/../public/special_banner_image_1.png",
+    imageUrl: "/special_banner_image_1.png",
   },
   {
     title: "Rafine Şeker İçermez",
-    imageUrl: "/../public/special_banner_image_2.png",
+    imageUrl: "/special_banner_image_2.png",
   },
   {
     title: "Renklendirici İçermez",
-    imageUrl: "/../public/special_banner_image_3.png",
+    imageUrl: "/special_banner_image_3.png",
   },
   {
     title: "Vegan",
-    imageUrl: "/../public/special_banner_image_4.png",
+    imageUrl: "/special_banner_image_4.png",
   },
   {
     title: "Glutensiz",
-    imageUrl: "/../public/special_banner_image_5.png",
+    imageUrl: "/special_banner_image_5.png",
   },
   {
     title: "Lezzet Dolu",
-    imageUrl: "/../public/special_banner_image_6.png",
+    imageUrl: "/special_banner_image_6.png",
   },
 ];
--- public/popularProducts.js
+++ public/popularProducts.js
@@ -2,41 +2,41 @@ export const popularProducts = [
   {
     title: "9'lu Tanışma Paketi",
     price: "265,00 TL",
-    photoUrl: "/../public/11.jpeg",
+    photoUrl: "/11.jpeg",
   },
   {
     title: "Glutensiz Parti Paketi",
     price: "169,00 TL",
-    photoUrl: "/../public/12.jpeg",
+    photoUrl: "/12.jpeg",
   },
   {
     title: "15'li Mega Paket",
     price: "545,00 TL",
-    photoUrl: "/../public/13.jpeg",
+    photoUrl: "/13.jpeg",
   },
   {
     title: "Glutensiz Kahvaltı Paketi",
     price: "168,00 TL",
-    photoUrl: "/../public/14.jpeg",
+    photoUrl: "/14.jpeg",
   },
   {
     title: "3'lü Çikolata + Çilek ",
     price: "207,90 TL",
-    photoUrl: "/../public/15.jpeg",
+    photoUrl: "/15.jpeg",
   },
   {
     title: "3'lü Kakao + Yer Fıstığı ",
     price: "207,90 TL",
-    photoUrl: "/../public/16.jpeg",
+    photoUrl: "/16.jpeg",
   },
   {
     title: "3'lü Muz + Tarçın ",
     price: "207,90 TL",
-    photoUrl: "/../public/17.jpeg",
+    photoUrl: "/17.jpeg",
   },
   {
     title: "10'lu Paket Kakao + Yer Fıstığı ",
     price: "350,00 TL TL",
-    photoUrl: "/../public/18.jpeg",
+    photoUrl: "/18.jpeg",
   },
 ];
