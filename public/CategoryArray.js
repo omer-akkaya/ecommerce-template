f8303c369c3e11ca13c071c815268025d9b4abb6
omer akkaya <farukakkayaya51@gmail.com>
2023-04-27 11:12:14 +0300

    added public folder

--- public/CategoryArray.js
+++ public/CategoryArray.js
@@ -1,11 +1,46 @@
 const CategoryArray = [
-  { id: 1, title: "Avantajlı Paketler", image: "/../public/cipss.png" },
-  { id: 2, title: "Mix Paketler", image: "/../public/cipss.png" },
-  { id: 3, title: "Granola & Yulaf Ezmesi", image: "/../public/cipss.png" },
-  { id: 4, title: "Kinoa Cips", image: "/../public/cipss.png" },
-  { id: 5, title: "Raw Bites", image: "/../public/cipss.png" },
-  { id: 6, title: "Cracks", image: "/../public/cipss.png" },
-  { id: 7, title: "Glutensiz Unlar", image: "/../public/cipss.png" },
+  {
+    id: 1,
+    title: "Avantajlı Paketler",
+    image: "/../public/cracks_icon.png",
+    shorts: "/../public/shorts_1.png",
+  },
+  {
+    id: 2,
+    title: "Mix Paketler",
+    image: "/../public/mix_icon.png",
+    shorts: "/../public/shorts_2.png",
+  },
+  {
+    id: 3,
+    title: "Granola & Yulaf Ezmesi",
+    image: "/../public/granola_icon.png",
+    shorts: "/../public/shorts_3.png",
+  },
+  {
+    id: 4,
+    title: "Kinoa Cips",
+    image: "/../public/cipss.png",
+    shorts: "/../public/shorts_4.png",
+  },
+  {
+    id: 5,
+    title: "Raw Bites",
+    image: "/../public/raw_bites_icon.png",
+    shorts: "/../public/shorts_5.png",
+  },
+  {
+    id: 6,
+    title: "Cracks",
+    image: "/../public/cracks_icon.png",
+    shorts: "/../public/shorts_6.png",
+  },
+  {
+    id: 7,
+    title: "Glutensiz Unlar",
+    image: "/../public/un_icon.png",
+    shorts: "/../public/shorts_1.png",
+  },
 ];
 
 export default CategoryArray;
--- public/popularProducts.js
+++ public/popularProducts.js
@@ -1,6 +1,42 @@
 export const popularProducts = [
-  { title: "9'lu Tanışma Paketi", price: "265,00 TL", photoUrl: "" },
-  { title: "Glutensiz Parti Paketi", price: "169,00 TL", photoUrl: "" },
-  { title: "15'li Mega Paket", price: "545,00 TL", photoUrl: "" },
-  { title: "Glutensiz Kahvaltı Paketi", price: "168,00 TL", photoUrl: "" },
+  {
+    title: "9'lu Tanışma Paketi",
+    price: "265,00 TL",
+    photoUrl: "/../public/11.jpeg",
+  },
+  {
+    title: "Glutensiz Parti Paketi",
+    price: "169,00 TL",
+    photoUrl: "/../public/12.jpeg",
+  },
+  {
+    title: "15'li Mega Paket",
+    price: "545,00 TL",
+    photoUrl: "/../public/13.jpeg",
+  },
+  {
+    title: "Glutensiz Kahvaltı Paketi",
+    price: "168,00 TL",
+    photoUrl: "/../public/14.jpeg",
+  },
+  {
+    title: "3'lü Çikolata + Çilek ",
+    price: "207,90 TL",
+    photoUrl: "/../public/15.jpeg",
+  },
+  {
+    title: "3'lü Kakao + Yer Fıstığı ",
+    price: "207,90 TL",
+    photoUrl: "/../public/16.jpeg",
+  },
+  {
+    title: "3'lü Muz + Tarçın ",
+    price: "207,90 TL",
+    photoUrl: "/../public/17.jpeg",
+  },
+  {
+    title: "10'lu Paket Kakao + Yer Fıstığı ",
+    price: "350,00 TL TL",
+    photoUrl: "/../public/18.jpeg",
+  },
 ];
