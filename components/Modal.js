79978e2c4d40161b465bb0f246ce43c3d9d0acce
omer akkaya <farukakkayaya51@gmail.com>
2023-04-27 11:10:25 +0300

    modified modal

--- components/Modal.js
+++ components/Modal.js
@@ -9,14 +9,15 @@ const Modal = () => {
   }
   return (
     <div
+      data
       style={{ backgroundColor: "rgba(0, 0, 0, 0.7)" }}
-      className={`h-full w-full fixed z-40 left-0 top-0 flex justify-center items-center select-none ${
+      className={`h-full w-full fixed z-10 left-0 top-0 flex justify-center items-center select-none ${
         modalShown ? "" : "hidden"
       }`}
       onClick={clickHandler}
     >
-      <div className="flex flex-col z-50 bg-white justify-center items-center p-3 rounded-3xl shadow-xl relative">
-        <GiCancel className="absolute top-0 right-0 text-6xl m-2 hover:bg-sky-500 rounded-full cursor-pointer" />
+      <div className="flex flex-col z-20 bg-white justify-center items-center p-3 rounded-3xl shadow-xl relative">
+        <GiCancel className="absolute top-0 right-0 text-6xl m-2 hover:bg-red-500 rounded-full cursor-pointer" />
         <Image src={"/../public/shorts_1.png"} width={500} height={500} />
         <div className="py-5 text-2xl font-semibold">
           TÜM ÜRÜNLERDE %25 İNDİRİM
